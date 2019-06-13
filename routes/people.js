f90d2f4ed7267762555a5356e1cb8199d5157bbc
limadani <danieleddelima@gmail.com>
2019-06-13 11:51:36 -0300

    Exercício concluido

--- routes/people.js
+++ routes/people.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const db = require('../db');
-const router = express.Router();
+/*______________________________________________________
+* Centro Federal de Educação Tecnológica de Minas Gerais
+* Programação WEB: Zombie Garden
+* Aluna: Daniele de Souza Lima
+_______________________________________________________*/
+
+var express = require('express');
+var db = require('../db');
+
+var router = express.Router();
 
 /* GET lista de pessoas. */
-router.get('/', (req, res, next) => {
+router.get('/', function(req, res, next) {
 
   db.query({
     sql: 'SELECT * FROM person LEFT OUTER JOIN zombie ON eatenBy = zombie.id',
@@ -11,12 +18,8 @@ router.get('/', (req, res, next) => {
     // nas quais fizemos JOIN (neste caso, `person` e `zombie`).
     // descrição: https://github.com/felixge/node-mysql#joins-with-overlapping-column-names
     nestTables: true
-  }, (err, rows) => {
-      if (err) {
-        res.status(500)
-          .send('Problema ao recuperar pessoas. Descrição: ' + err);
-      }
-
+    }, function(err, rows) {
+      if (err) res.status(500).send('Problema ao recuperar pessoas.');
       // renderiza a view de listagem de pessoas, passando como contexto
       // de dados:
       // - people: com um array de `person`s do banco de dados
@@ -34,14 +37,12 @@ router.get('/', (req, res, next) => {
 
 
 /* PUT altera pessoa para morta por um certo zumbi */
-router.put('/eaten/', (req, res) => {
+router.put('/eaten/', function(req, res) {
   db.query('UPDATE person ' +
            'SET alive = false, eatenBy = ' + db.escape(req.body.zombie) + ' ' +
            'WHERE id = ' + db.escape(req.body.person),
-    (err, result) => {
-      if (err) {
-        req.flash('error', 'Erro desconhecido. Descrição: ' + err);
-      } else if (result.affectedRows !== 1) {
+    function(err, result) {
+      if (result.affectedRows !== 1) {
         req.flash('error', 'Nao ha pessoa para ser comida');
       } else {
         req.flash('success', 'A pessoa foi inteiramente (nao apenas cerebro) engolida.');
@@ -52,11 +53,9 @@ router.put('/eaten/', (req, res) => {
 
 
 /* GET formulario de registro de nova pessoa */
-router.get('/new/', (req, res) => {
+router.get('/new/', function(req, res) {
   res.render('newPerson');
 });
-
-
 
 
 /* POST registra uma nova pessoa */
@@ -66,7 +65,19 @@ router.get('/new/', (req, res) => {
 //   2. Redirecionar para a rota de listagem de pessoas
 //      - Em caso de sucesso do INSERT, colocar uma mensagem feliz
 //      - Em caso de erro do INSERT, colocar mensagem vermelhinha
+router.post('/', function(req, res){
+  db.query("INSERT INTO `zombies`.`person` (name) VALUES("+ db.escape(req.body.name) +")",
+    function(err, result){
+      if (err){
+        res.status(500).send('Erro ao criar pessoa');
+        return;
+      }
+      req.flash('peopleCountChange', '+1');
+      req.flash('success', 'Uma nova pessoa criada: ' + req.body.name);
+      res.redirect('/people');
+    })
 
+});
 
 /* DELETE uma pessoa */
 // Exercício 2: IMPLEMENTAR AQUI
@@ -75,7 +86,17 @@ router.get('/new/', (req, res) => {
 //   2. Redirecionar para a rota de listagem de pessoas
 //      - Em caso de sucesso do INSERT, colocar uma mensagem feliz
 //      - Em caso de erro do INSERT, colocar mensagem vermelhinha
-
-
+router.delete('/:id', function(req, res){
+  db.query('DELETE FROM zombies.person WHERE id = ' + req.params.id,
+  function(err, result){
+    if (err) {
+      res.status(500).send('Erro ao deletar uma pessoa.');
+      return;
+    }
+    req.flash('peopleCountChange', '-1');
+    req.flash('success', 'Pessoa excluida');
+    res.redirect('/people');
+  });
+});
 
 module.exports = router;
